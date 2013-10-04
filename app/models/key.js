87d8739ddb1a7d287eb9b59e30706dbe0b36fb00
Hashrocket Workstation <dev@hashrocket.com>
2013-10-04 16:48:13 -0400

    Add Key model and a fixture

--- app/models/key.js
+++ app/models/key.js
@@ -1,6 +1,17 @@
-App.Key = DS.Model.extend({
+var Key = DS.Model.extend({
   keycode: DS.attr("number"),
   primary: DS.attr("string"),
   side: DS.attr("string"),
   alt: DS.attr("string")
 });
+
+Key.FIXTURES = [
+  {
+    side: "f5",
+    primary: "5",
+    keycode: 59,
+    alt: "%"
+  }
+];
+
+export default Key;
